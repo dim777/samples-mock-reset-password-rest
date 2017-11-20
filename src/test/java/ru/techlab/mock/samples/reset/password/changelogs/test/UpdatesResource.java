18f7792bc6322774fb4191d19e214165ce04e93d
dim777 <dim777@ya.ru>
2017-11-20 19:38:51 +0300

    Working on change password

---
+++ src/test/java/ru/techlab/mock/samples/reset/password/changelogs/test/UpdatesResource.java
@@ -0,0 +1,25 @@
+package ru.techlab.mock.samples.reset.password.changelogs.test;
+
+import com.github.mongobee.changeset.ChangeLog;
+import com.github.mongobee.changeset.ChangeSet;
+import org.springframework.context.ApplicationContext;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import reactor.core.publisher.Flux;
+import ru.techlab.mock.samples.reset.password.model.User;
+import ru.techlab.mock.samples.reset.password.repository.UserRepository;
+
+@ChangeLog(order = "1")
+public class UpdatesResource {
+  @ChangeSet(author = "dim777", id = "init", order = "001")
+  public void init(MongoTemplate template, ApplicationContext applicationContext) {
+    UserRepository userRepository = (UserRepository)applicationContext.getBean("userRepository");
+
+    userRepository
+            .saveAll(Flux.just(
+                    User.getBuilder().account("tzrb052775").name("D Test 0").password("111222").build(), //
+                    User.getBuilder().account("tzrb052776").name("D Test 1").password("111222").build(), //
+                    User.getBuilder().account("tzrb052777").name("D Test 2").password("111222").build())) //
+            .then() //
+            .block();
+  }
+}
